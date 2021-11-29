5972c6cc00aec29b245a3f08d3aae82a882277b8
selcyc <antuan1098216@gmail.com>
2021-11-29 12:40:01 -0700

    react11

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import moment from "moment";
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
@@ -8,9 +9,27 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import contact from "./pages/contact";
 import blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor(){
+    super();
+
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
+  }
+  getPortfolioItems(){
+    axios
+      .get("https://selcyc.devcamp.space/portfolio/portfolio_items5")
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
+    this.getPortfolioItems();
     return (
       <div className="app">
         <Router>
@@ -24,6 +43,8 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={contact} />
               <Route path="/blog" component={blog} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail}/>
+              <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
---
+++ src/components/pages/no-match.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+export default function() {
+  return(
+        <div>
+            <h2>we come in peace</h2>
+            <Link to="/">Return to homepage</Link>
+        </div>
+    );
+}
